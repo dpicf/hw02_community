befe0dd62de27f5ada281f53168c1c3aa0b66279
Дмитрий Игошев <digoschev@yandex.ru>
2021-04-23 17:58:33 +0300

    add in admin.py GroupAdmin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+
+from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,4 +10,9 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "description",)
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
